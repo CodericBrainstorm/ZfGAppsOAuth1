1e7bef340d5a596a77885568e729041a3693c208
Neftali Yagua <neftali.yagua@gmail.com>
2013-04-04 16:56:00 -0430

    Enrutamiento y Controlador de authenticacin

--- src/GAppsOAuth1/Controller/AuthController.php
+++ src/GAppsOAuth1/Controller/AuthController.php
@@ -10,6 +10,12 @@
 namespace GAppsOAuth1\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Auth\OpenID\google_discovery;
+use Auth\OpenID\AX;
+use Auth\OpenID\Consumer;
+use Auth\OpenID\FileStore;
+use Auth\OpenID\PAPE;
+
 class AuthController extends AbstractActionController
 {
     public function loginAction()
