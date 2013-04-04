ea933c015807ea90fde428d8961fd38bd2ad3eb3
Neftali Yagua <neftali.yagua@gmail.com>
2013-04-04 14:58:26 -0430

    Creando Modulo OAuth1

---
+++ Module.php
@@ -0,0 +1,40 @@
+<?php
+namespace GAppsOAuth1;
+
+use Reservas\Model\Localizador;
+use Reservas\Model\LocalizadorTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module
+{
+        // Add this method:
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+		'GAppsOAuth1\Model\GAppsOAuth1Storage' => function($sm){
+		    return new \GAppsOAuth1\Model\GAppsOAuth1Storage('GAppsOAuth1');  
+		},
+            ),
+        );
+    }
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
---
+++ src/GAppsOAuth1/Model/GAppsOAuth1Storage.php
@@ -0,0 +1,16 @@
+<?php
+namespace GAppsOAuth1\Model;
+use Zend\Authentication\Storage;
+class GAppsOAuth1Storage extends Storage\Session
+{
+    public function setRememberMe($rememberMe = 0, $time = 1209600)
+    {
+        if ($rememberMe == 1) {
+            $this->session->getManager()->rememberMe($time);
+        }
+    }
+    public function forgetMe()
+    {
+        $this->session->getManager()->forgetMe();
+    } 
+}
