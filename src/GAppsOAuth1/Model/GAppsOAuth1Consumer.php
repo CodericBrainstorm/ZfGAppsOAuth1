eab9440109cb328204d7da853c673dee4161a41c
Neftali Yagua <neftali.yagua@gmail.com>
2013-04-07 05:07:19 -0430

    Agregar Clases

--- src/GAppsOAuth1/Model/GAppsOAuth1Consumer.php
+++ src/GAppsOAuth1/Model/GAppsOAuth1Consumer.php
@@ -4,8 +4,7 @@
  * @author Neftalí Yagua <neftali.yagua@gmail.com>
  */
 namespace Auth\OpenID;
-use Auth\OpenID;
-class GAppsOAuth1Consumer extends Consumer{
+class GAppsOAuth1Consumer extends Auth_OpenID_Consumer{
     public function __construct(){
         $STORE_PATH = "/tmp/_php_consumer_test";
         if (!file_exists($STORE_PATH) && !mkdir($STORE_PATH)) {
