da656506fa011aed8b7cca1a88dbd4be928cd656
Neftali Yagua <neftali.yagua@gmail.com>
2013-04-28 09:43:07 -0430

    Cambio bsico

--- src/GAppsOAuth1/Model/GAppsOAuth1Consumer.php
+++ src/GAppsOAuth1/Model/GAppsOAuth1Consumer.php
@@ -4,7 +4,7 @@
  * @author Neftalí Yagua <neftali.yagua@gmail.com>
  */
 namespace Auth\OpenID;
-class GAppsOAuth1Consumer extends Auth_OpenID_Consumer{
+class GAppsOAuth1Consumer extends Consumer{
     public function __construct(){
         $STORE_PATH = "/tmp/_php_consumer_test";
         if (!file_exists($STORE_PATH) && !mkdir($STORE_PATH)) {
